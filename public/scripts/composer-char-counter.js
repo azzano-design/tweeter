a1932659ab4cb34a876b2fd8e4ffeefd74622a32
Brendan <brendanarchangelo@gmail.com>
2017-12-12 19:52:23 +0000

    Added counting function, and warning when limit is exceeded

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-  console.log('Counter ready to go');
+  $('.new-tweet').on('keyup', 'textarea', function() {
+    var charCount = +$(this).val().length;
+    if (charCount > 140) {
+      $('.counter').addClass('invalid');
+    } else {
+      $('.counter').removeClass('invalid');
+    }
+    $('.counter').text(charCount);
+   });
 });
